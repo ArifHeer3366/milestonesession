2da2385be74991df41a5efa5c081934eaf8e4da5
Arif Heer <arif.heer3366@gmail.com>
2019-09-05 16:19:27 +0500

    m7 added for version-7

--- src/main/java/com/milestonesession/milestonesession/info.java
+++ src/main/java/com/milestonesession/milestonesession/info.java
@@ -3,5 +3,20 @@ package com.milestonesession.milestonesession;
 public class info {
     private String git;
     private String session;
-    
+
+    public String getGit() {
+        return git;
+    }
+
+    public void setGit(String git) {
+        this.git = git;
+    }
+
+    public String getSession() {
+        return session;
+    }
+
+    public void setSession(String session) {
+        this.session = session;
+    }
 }
