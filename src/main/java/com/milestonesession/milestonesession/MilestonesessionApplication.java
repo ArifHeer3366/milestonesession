942faa5ea1bcb869e2367dff4b15ca7d42114af0
Arif Heer <arif.heer3366@gmail.com>
2019-09-05 15:06:25 +0500

    m1 added for version-1

---
+++ src/main/java/com/milestonesession/milestonesession/MilestonesessionApplication.java
@@ -0,0 +1,13 @@
+package com.milestonesession.milestonesession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilestonesessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilestonesessionApplication.class, args);
+    }
+
+}
