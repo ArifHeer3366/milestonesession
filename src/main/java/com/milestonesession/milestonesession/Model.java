d2aa302de875c9fb8c6479f0f8a0076ced1b11c5
Arif Heer <arif.heer3366@gmail.com>
2019-09-05 15:59:51 +0500

    m6 added for version-6

--- src/main/java/com/milestonesession/milestonesession/Model.java
+++ src/main/java/com/milestonesession/milestonesession/Model.java
@@ -4,4 +4,19 @@ public class Model {
     private String session;
     private String session1;
 
+    public String getSession() {
+        return session;
+    }
+
+    public void setSession(String session) {
+        this.session = session;
+    }
+
+    public String getSession1() {
+        return session1;
+    }
+
+    public void setSession1(String session1) {
+        this.session1 = session1;
+    }
 }
